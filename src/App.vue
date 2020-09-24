5bca65ffb185aa8536dafb2acff1beef78488c0c
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2020-09-24 20:28:54 +0200

    added first animantions with cdn

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,28 @@
       <div class="face" @mousemove="eyeMove">
         <!-- <img class="ground-layer" :src="images.face" alt="">      -->
         <div class="eyes">
-          <img  :src="images.eyes" alt="">
-          <img  :src="images.pupilLeft" alt="">
-          <img  :src="images.pupilRight" alt="">
+          <!-- <img  :src="images.eyes" alt=""> -->
+          <div class="left-eye">
+            <img class="left-eye" :src="images.eyeLeft" alt="">
+            <img class="left-lid" :src="images.eyelidLeft" alt="">
+            <img class="left-pupil" :src="images.pupilLeft" alt="">
+          </div>
+          <div class="right-eye">
+            <img class="right-eye" :src="images.eyeRight" alt="">
+            <img class="right-lid" :src="images.eyelidRight" alt="">
+            <img class="right-pupil"  :src="images.pupilRight" alt="">
+          </div>
+          
         </div>
         <div class="nose">
           <img  :src="images.nose" alt="">
         </div>
-        <div class="mouth">
+        <div class="mouth" @click="koeka">
           <img :src="images.mouth" alt="">
         </div>
+        <div class="blush">
+          <img :src="images.blush" alt="">
+        </div>  
       </div> <!--end of face div -->
     </div><!--end of wrapper div --> 
   </div>
@@ -29,18 +41,24 @@ export default {
       msg: 'satan satan satan',
       images: {
         face: require('./assets/groundLayer.svg'),
-        eyes: require('./assets/eyes.svg'),
+        hair: require('./assets/hair.svg'),
+        eyeLeft: require('./assets/eyeLeft.svg'),
+        eyeRight: require('./assets/eyeRight.svg'),
         pupilLeft: require('./assets/pupilLeft2.svg'),
         pupilRight: require('./assets/pupilRight2.svg'),
+        eyelidLeft: require('./assets/eyelidLeft.svg'),
+        eyelidRight: require('./assets/eyelidRight.svg'),
         nose: require('./assets/nose.svg'),
-        mouth: require('./assets/mouth.svg')
+        snot: require('./assets/snot.svg'),
+        mouth: require('./assets/mouthNew.svg'),
+        blush: require('./assets/blush.svg')
       }
     }
   },
   methods: {
     eyeMove: function(e){
 
-      const eyes = document.querySelectorAll('.eyes img:nth-child(1n+2)');  
+      const eyes = document.querySelectorAll('.left-pupil, .right-pupil');  
 
       for(let i=0;i<eyes.length;i++){
         const x = eyes[i].getBoundingClientRect().left + (eyes[i].clientWidth / 2);
@@ -49,10 +67,54 @@ export default {
         const rot = (radian * (180 / Math.PI) * -1) + 0; //was +90 
         eyes[i].style.transform = `rotate(${rot}deg)`;
       }
-      
-      
-      
+    },
+    blink: function(){
+
+       const eyeLids = document.querySelectorAll('.left-lid, .right-lid');
+
+      for(let lid of eyeLids ){
+        setInterval(()=>{
+          if(lid.style.display === 'none'){
+            lid.style.display =  'block';
+          }else if(lid.style.display !== 'none'){
+            lid.style.display =  'none';
+          }
+          },3000);
+      }
+   
+    },
+    blush: function(){
+      const cheeks = document.querySelector('.blush');
+      const tl = gsap.timeline({repeat: -1, repeatDelay: 5, delay: 10});
+      //cheeks.style.display = 'none';
+
+      cheeks.style.opacity = 0;
+
+      // 
+
+      //setInterval(()=>{
+        tl.to(cheeks, {duration: 3, opacity: 1, delay: 10});
+        tl.to(cheeks, {duration: 3, opacity: 0, delay: 5});
+        
+
+        // gsap.to(cheeks, {duration: 3, opacity: 1});
+        // gsap.from(cheeks, {duration: 3, opacity: 0});
+
+        // if(cheeks.style.opacity = 0){
+        //   gsap.from(cheeks, {duration: 3, opacity: 1});
+        // }else if(cheeks.style.opacity = 1){
+        //   gsap.from(cheeks, {duration: 3, opacity: 0});
+        // }
+
+      //},3000)
+    },
+    koeka: function(){    
+      console.log('satan');
     }
+  },
+  mounted(){
+    this.blink();
+    this.blush();
   }
 }
 </script>
@@ -87,48 +149,103 @@ export default {
   background-size: contain; 
   display: grid;
   grid-template-columns: 40px 1fr 1fr 40px;
-  // grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-rows:  repeat(5, 1fr);
+  //height: 100vh;
+  // max-width: 720px;
+  // height: 40%;
+  //position: relative;
+  // .ground-layer{
+  //   grid-column: 1/5;
+  //   grid-row: 1/5;
+  //   width: 100%;
+  // }
+
   .eyes{
-    position: relative;
-    grid-column: 1 /4;
-    grid-row: 2/ 4;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img:nth-child(1){
-      position: absolute;
-      width: 100%;
-    }
-    img:nth-child(2){
-      position: absolute;
-      width: 2.8%;
-      top: 35%;
-      left: 35%;
-    }
-    img:nth-child(3){
-      position: absolute;
-      width: 2.8%;
-      top: 33%;
-      left: 65%;
-    }
-  }
-  .nose{
-    grid-column: 2 /4;
-    grid-row: 2/ 4;
-    width: 20%;
+     display: flex;
+    /* width: 50%; */
+    /* justify-content: flex-end; */
+    grid-column: 2/4;
+    grid-row: 2/3;
     justify-self: center;
     align-self: center;
+    //border: 2px solid pink;
+  }
+
+  .left-eye{
+    position: relative;
+    width: 100%;
+    /* display: flex; */
+    /* margin: 0 auto; */
+    display: flex;
+    justify-content: center;
+
+      img{
+      width: 30%;
+    }
+  }
+
+  .right-eye,
+  .left-eye{
+    width: 100%;
+    display: flex;
+    position: relative;
+    justify-content: center;
+    img{
+      width: 45%;
+    }
+     .right-pupil{
+      position: absolute;
+      width: 15%;
+      // top: 33%;
+      // left: 65%;
+    }
+     .left-pupil{
+      position: absolute;
+      width: 15%;
+      // top: 35%;
+      // left: 35%;
+    }
+  }
+
+
+
+   
+    .left-lid,
+    .right-lid{
+      position: absolute;
+      // top: -3%;
+      // left: 0;
+      z-index: 9999;
+    }
+  
+  .nose{
+    grid-column: 2/4;
+    grid-row: 2/4;
+    justify-self: center;
+    align-self: center;
+    width: 25%;
+    margin-top: 40px;
     img{
       width: 100%;
     }
   }
   .mouth{
+    top: 60%;
     grid-column: 2 /4;
-    grid-row: 2/4;
-    //width: 70%;
+    grid-row: 4/5;
     justify-self: center;
+    align-self: center;
+    width: 40%;
     img{
       width: 100%;
+    }
+  }
+  .blush{
+    grid-column: 2/4;
+    grid-row: 3/4;
+    align-self: end;
+    img{
+      width: 80%;
     }
   }
   }
